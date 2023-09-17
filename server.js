55984ce3ca56afe49d97afcea9d439c1457e474d
Jrive10 <juliorivera1993@icloud.com>
2023-09-17 15:32:30 -0400

    still trying

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 require('dotenv').config(); // Load environment variables from .env file
 
+app.listen(process.env.PORT || 3000);
 const PORT = process.env.PORT || 3000;
 const mongoURI = process.env.MONGO_URI;
 
